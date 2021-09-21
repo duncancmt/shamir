5a2c10dd43b4186b2bfda8517f3d2013a95269c7
Duncan Townsend <duncan@immunefi.com>
2021-09-21 11:59:34 -0400

    Split functionality into BinaryPolynomial

--- gf.py
+++ gf.py
@@ -1,4 +1,4 @@
-from typing import Optional, TypeVar, Type, Tuple, Union, Literal
+from typing import Optional, TypeVar, Type, Tuple, Union, Literal, NoReturn
 from types import TracebackType
 
 _default_prim_poly = {
@@ -26,6 +26,8 @@ class Context(object):
 
     def copy(self: SelfType) -> SelfType:
         return type(self)(self.bit_length, self.modulus)
+
+    del SelfType
 
 
 _current_context: contextvars.ContextVar[Context] = contextvars.ContextVar("gf_context")
@@ -80,42 +82,52 @@ class _ContextManager(object):
         return False
 
 
-class Element(object):
-    __slots__ = "_value", "_bit_length", "_modulus"
+class BinaryPolynomial(object):
+    __slots__ = ("_value",)
     _value: int
-    _bit_length: int
-    _modulus: int
 
-    SelfType = TypeVar("SelfType", bound="Element")
+    SelfType = TypeVar("SelfType", bound="BinaryPolynomial")
 
-    def __init__(self, value: int, context: Optional[Context] = None) -> None:
+    def __init__(self, value: int) -> None:
+        if value < 0:
+            value = -value
         self._value = value
-        if context is None:
-            context = getcontext()
-        self._bit_length = context.bit_length
-        self._modulus = context.modulus
 
     def _coerce(self: SelfType, other: Union[SelfType, int]) -> SelfType:
         if isinstance(other, int):
-            return type(self)(other, Context(self._bit_length, self._modulus))
-        context = getcontext()
-        if self._bit_length != context.bit_length or self._modulus != context.modulus:
-            raise RuntimeError("Wrong context")
-        if self._bit_length != other._bit_length or self._modulus != other._modulus:
-            raise TypeError("Different fields")
+            return type(self)(other)
         return other
 
     def __add__(self: SelfType, other: Union[SelfType, int]) -> SelfType:
         other = self._coerce(other)
-        return type(self)(
-            self._value ^ other._value, Context(self._bit_length, self._modulus)
-        )
+        return type(self)(self._value ^ other._value)
+
+    def __radd__(self: SelfType, other: int) -> SelfType:
+        return type(self)(other) + self
 
     def __neg__(self: SelfType) -> SelfType:
         return self
 
     def __sub__(self: SelfType, other: Union[SelfType, int]) -> SelfType:
         return self + -other
+
+    def __rsub__(self: SelfType, other: int) -> SelfType:
+        return type(self)(other) + -self
+
+    def __mul__(self: SelfType, other: Union[SelfType, int]) -> SelfType:
+        other = self._coerce(other)
+        a = self._value
+        b = other._value
+        p = 0
+        while a and b:
+            if b & 1:
+                p ^= a
+            b >>= 1
+            a <<= 1
+        return type(self)(p)
+
+    def __rmul__(self: SelfType, other: int) -> SelfType:
+        return type(self)(other) * self
 
     def __divmod__(
         self: SelfType, other: Union[SelfType, int]
@@ -129,19 +141,109 @@ class Element(object):
             shift = remainder.bit_length() - denominator.bit_length()
             quotient ^= 1 << shift
             remainder ^= denominator << shift
-        context = Context(self._bit_length, self._modulus)
-        return type(self)(quotient, context), type(self)(remainder, context)
+        return type(self)(quotient), type(self)(remainder)
 
-    def __mul__(self: SelfType, other: Union[SelfType, int]) -> SelfType:
+    def __rdivmod__(self: SelfType, other: int) -> Tuple[SelfType, SelfType]:
+        return divmod(type(self)(other), self)
+
+    def __floordiv__(self: SelfType, other: Union[SelfType, int]) -> SelfType:
+        return divmod(self, other)[0]
+
+    def __rfloordiv__(self: SelfType, other: int) -> SelfType:
+        return type(self)(other) // self
+
+    def __mod__(self: SelfType, other: Union[SelfType, int]) -> SelfType:
+        return divmod(self, other)[1]
+
+    def __rmod__(self: SelfType, other: int) -> SelfType:
+        return type(self)(other) % self
+
+    def __div__(self: SelfType, other: Union[SelfType, int]) -> SelfType:
+        return NotImplemented
+
+    def __rdiv__(self: SelfType, other: int) -> NoReturn:
+        raise NotImplementedError
+
+    def __int__(self) -> int:
+        return self._value
+
+    def __bool__(self) -> bool:
+        return bool(int(self))
+
+    def __str__(self) -> str:
+        return f"BinaryPolynomial({bin(self._value)})"
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, (int, BinaryPolynomial)):
+            other = self._coerce(other)
+            return self._value == other._value
+        return False
+
+    del SelfType
+
+
+class Element(object):
+    __slots__ = "_value", "_modulus"
+    _value: BinaryPolynomial
+    _modulus: BinaryPolynomial
+
+    SelfType = TypeVar("SelfType", bound="Element")
+
+    def __init__(
+        self,
+        value: Union[BinaryPolynomial, int],
+        modulus: Union[BinaryPolynomial, int, None] = None,
+    ) -> None:
+        if isinstance(value, int):
+            value = BinaryPolynomial(value)
+        self._value = value
+        if modulus is None:
+            modulus = getcontext().modulus
+        if isinstance(modulus, int):
+            modulus = BinaryPolynomial(modulus)
+        self._modulus = modulus
+
+    def _coerce(
+        self: SelfType, other: Union[SelfType, BinaryPolynomial, int]
+    ) -> SelfType:
+        if isinstance(other, (int, BinaryPolynomial)):
+            other = type(self)(other, self._modulus)
+        if self._modulus != other._modulus:
+            raise ValueError("Different fields")
+        return other
+
+    def __add__(
+        self: SelfType, other: Union[SelfType, BinaryPolynomial, int]
+    ) -> SelfType:
         other = self._coerce(other)
-        bit_length = self._bit_length
-        modulus = self._modulus
-        selector = 1 << bit_length
+        return type(self)(self._value + other._value, self._modulus)
+
+    def __radd__(self: SelfType, other: Union[BinaryPolynomial, int]) -> SelfType:
+        return type(self)(other, self._modulus) + self
+
+    def __neg__(self: SelfType) -> SelfType:
+        return self
+
+    def __sub__(
+        self: SelfType, other: Union[SelfType, BinaryPolynomial, int]
+    ) -> SelfType:
+        return self + -other
+
+    def __rsub__(self: SelfType, other: Union[BinaryPolynomial, int]) -> SelfType:
+        return type(self)(other, self._modulus) - self
+
+    def __mul__(
+        self: SelfType, other: Union[SelfType, BinaryPolynomial, int]
+    ) -> SelfType:
+        other = self._coerce(other)
+        modulus = self._modulus._value
+        selector = 1 << (modulus.bit_length() - 1)
         mask = selector - 1
-        a = self._value
-        b = other._value
+        modulus &= mask
+        a = self._value._value
+        b = other._value._value
         p = 0
-        for _ in range(bit_length):
+        while a and b:
             if b & 1:
                 p ^= a
             b >>= 1
@@ -149,21 +251,25 @@ class Element(object):
             if a & selector:
                 a ^= modulus
                 a &= mask
-        return type(self)(p, Context(bit_length, modulus))
+        return type(self)(p, self._modulus)
 
-    def __div__(self: SelfType, other: Union[SelfType, int]) -> SelfType:
+    def __rmul__(self: SelfType, other: Union[BinaryPolynomial, int]) -> SelfType:
+        return type(self)(other, self._modulus) * self
+
+    def __truediv__(
+        self: SelfType, other: Union[SelfType, BinaryPolynomial, int]
+    ) -> SelfType:
+        other = self._coerce(other)
         return self * ~other
 
     def __invert__(self: SelfType) -> SelfType:
-        modulus = (1 << self._bit_length) | self._modulus
-        context = Context(self._bit_length, self._modulus)
-        t = type(self)(0, context)
-        newt = type(self)(1, context)
-        r = type(self)(modulus, context)
-        newr = self
+        modulus = self._modulus
+        t: Union[BinaryPolynomial, int] = 0
+        newt: Union[BinaryPolynomial, int] = 1
+        r = modulus
+        newr = self._value
         while newr:
-            quotient = divmod(r, newr)[0]
-            # TODO: this needs to be polynomial multiplication, not field multiplication
+            quotient = r // newr
             r, newr = newr, r - quotient * newr
             t, newt = newt, t - quotient * newt
             print("quotient:", quotient)
@@ -173,32 +279,28 @@ class Element(object):
         #     raise RuntimeError("zero element or modulus is not irreducible")
         # if t > modulus:
         #     t = divmod(t, modulus)[1]
-        return t
+        return type(self)(t, self._modulus)
 
     def __bool__(self) -> bool:
-        return (
-            divmod(
-                self,
-                type(self)(
-                    (1 << self._bit_length) | self._modulus,
-                    Context(self._bit_length, self._modulus),
-                ),
-            )[1]
-            != 0
-        )
+        return self._value % self._modulus != 0
 
     def __str__(self) -> str:
         context = getcontext()
-        if self._bit_length == context.bit_length and self._modulus == context.modulus:
-            return f"Element({bin(self._value)})"
+        if self._modulus == context.modulus:
+            return f"Element({bin(int(self._value))})"
         else:
-            return f"Element({bin(self._value)} over {bin((1 << self._bit_length) | self._modulus)})"
+            return f"Element({bin(int(self._value))} over {bin(int(self._modulus))})"
+
+    def __int__(self) -> int:
+        return int(self._value)
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, (int, Element)):
             other = self._coerce(other)
             return self._value == other._value
         return False
+
+    del SelfType
 
 
 # Don't contaminate the namespace
