af2f81b4eda7c2e9b25113db0f3f569c76906183
Duncan Townsend <duncan@immunefi.com>
2021-09-25 11:18:56 -0400

    Better end-to-end SSS test

--- tests/test_shamir.py
+++ tests/test_shamir.py
@@ -8,21 +8,18 @@ import shamir
 
 class TestShamir(unittest.TestCase):
     def setUp(self) -> None:
-        self.modulus = gf.get_modulus(16)
-        self.secret = gf.ModularBinaryPolynomial(
-            secrets.randbits(self.modulus.bit_length() - 1), self.modulus
-        )
-        self.k = 5
-        self.n = 16
-        self.version = 1
+        self.modulus = gf.get_modulus(256)
 
     def test_shamir(self) -> None:
-        for i in range(1000):
-            with self.subTest(i=i):
-                shares = shamir.split(
-                    self.secret, self.k, self.n, self.version
-                )
-                self.assertEqual(len(shares), self.n)
-                shares = random.sample(shares, self.k)
-                recovered = shamir.recover(shares, self.version)
-                self.assertEqual(int(recovered), int(self.secret))
+        for k in range(1, 9):
+            for n in range(k + 1, 128, 8):
+                for version in range(10):
+                    with self.subTest(k=k, n=n, version=version):
+                        secret = gf.ModularBinaryPolynomial(
+                            secrets.randbits(self.modulus.bit_length() - 1), self.modulus
+                        )
+                        shares = shamir.split(secret, k, n, version)
+                        self.assertEqual(len(shares), n)
+                        shares = random.sample(shares, k)
+                        recovered = shamir.recover(shares, version)
+                        self.assertEqual(int(recovered), int(secret))
