d496cab0de4881b700ef707d6752e672da3edd53
Duncan Townsend <duncan@immunefi.com>
2021-09-25 19:04:50 -0400

    isort

--- gf.py
+++ gf.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 import secrets
+import warnings
 from types import TracebackType
 from typing import Generic, Literal, Optional, Type, TypeVar, Union, overload
-import warnings
 
 _default_prim_poly = {
     # these excessively-large moduli are provided for future applications which
