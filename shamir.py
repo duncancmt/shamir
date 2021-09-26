e00b91342c6737f40beda80d8ba64f2b7011739c
Duncan Townsend <duncan@immunefi.com>
2021-09-26 16:40:53 -0400

    blacken

--- shamir.py
+++ shamir.py
@@ -71,7 +71,9 @@ def split(
     if k < 1:
         raise ValueError(f"Can't split into {k} shares")
     if n < k:
-        raise ValueError(f"Requested {n} shares which is fewer than {k} required to recover")
+        raise ValueError(
+            f"Requested {n} shares which is fewer than {k} required to recover"
+        )
     # from high degree to low degree
     coeffs = tuple(_random_elements(secret, k - 1, nonce)) + (secret,)
     result: list[tuple[GFE, GFE]] = []
