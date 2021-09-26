b7d0347b9f719ade425c67dbce87eed22990fca6
Duncan Townsend <duncan@immunefi.com>
2021-09-26 16:37:55 -0400

    Useless to test with a single share

--- tests/test_shamir.py
+++ tests/test_shamir.py
@@ -10,8 +10,18 @@ class TestShamir(unittest.TestCase):
     def setUp(self) -> None:
         self.modulus = gf.get_modulus(256)
 
+    def test_degenerate(self) -> None:
+        for nonce in range(10):
+            with self.subTest(nonce=nonce):
+                secret = gf.ModularBinaryPolynomial(
+                    secrets.randbits(self.modulus.bit_length() - 1),
+                    self.modulus,
+                )
+                shares = shamir.split(secret, 1, 1, nonce)
+                self.assertEqual(int(shares[0][1]), int(secret))
+
     def test_shamir(self) -> None:
-        for k in range(1, 9):
+        for k in range(2, 9):
             for n in range(k + 1, 128, 8):
                 for nonce in range(10):
                     with self.subTest(k=k, n=n, nonce=nonce):
