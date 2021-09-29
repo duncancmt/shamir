25c93509444fd39cc6076ae53b5bc56c6992d707
Duncan Townsend <duncan@immunefi.com>
2021-09-29 16:00:48 -0400

    Update test_shamir.py -- now it takes a very long time

--- tests/test_shamir.py
+++ tests/test_shamir.py
@@ -1,5 +1,5 @@
+import itertools
 import random
-import secrets
 import unittest
 
 import gf
@@ -11,26 +11,77 @@ class TestShamir(unittest.TestCase):
         self.modulus = gf.get_modulus(256)
 
     def test_degenerate(self) -> None:
-        for nonce in range(10):
-            with self.subTest(nonce=nonce):
+        for salt in range(10):
+            with self.subTest(salt=salt):
                 secret = gf.ModularBinaryPolynomial(
-                    secrets.randbits(self.modulus.bit_length() - 1),
+                    random.getrandbits(self.modulus.bit_length() - 1),
                     self.modulus,
                 )
-                shares = shamir.split(secret, 1, 1, nonce)
-                self.assertEqual(int(shares[0][1]), int(secret))
+                shares, v, c = shamir.split(secret, 1, 1, salt)
+                self.assertEqual(int(shares[0]), int(secret))
 
-    def test_shamir(self) -> None:
-        for k in range(2, 9):
-            for n in range(k + 1, 128, 8):
-                for nonce in range(10):
-                    with self.subTest(k=k, n=n, nonce=nonce):
+    def test_verify(self) -> None:
+        for salt in range(10):
+            for k in range(2, 9):
+                for n in range(k, 24, 4):
+                    with self.subTest(k=k, n=n, salt=salt):
                         secret = gf.ModularBinaryPolynomial(
-                            secrets.randbits(self.modulus.bit_length() - 1),
+                            random.getrandbits(self.modulus.bit_length() - 1),
                             self.modulus,
                         )
-                        shares = shamir.split(secret, k, n, nonce)
-                        self.assertEqual(len(shares), n)
-                        shares = random.sample(shares, k)
-                        recovered = shamir.recover(shares, nonce)
-                        self.assertEqual(int(recovered), int(secret))
+                        shares, v, c = shamir.split(secret, k, n, salt)
+                        for i, share in enumerate(shares):
+                            with self.subTest(i=i):
+                                self.assertEqual(
+                                    shamir.verify(share, v, c), i + 1
+                                )
+
+    def test_recover(self) -> None:
+        for salt in range(10):
+            for k in range(2, 9):
+                for n in range(k, 128, 8):
+                    with self.subTest(k=k, n=n, salt=salt):
+                        secret = gf.ModularBinaryPolynomial(
+                            random.getrandbits(self.modulus.bit_length() - 1),
+                            self.modulus,
+                        )
+                        shares, v, c = shamir.split(secret, k, n, salt)
+                        for i in range(10):
+                            with self.subTest(i=i):
+                                subset = random.sample(shares, k)
+                                self.assertEqual(
+                                    int(shamir.recover(subset, v, c)),
+                                    int(secret),
+                                )
+
+    def test_lagrange_interpolate(self) -> None:
+        for salt in range(3):
+            for k in range(2, 9):
+                for n in range(k, 2 * k):
+                    with self.subTest(k=k, n=n, salt=salt):
+                        secret = gf.ModularBinaryPolynomial(
+                            random.getrandbits(self.modulus.bit_length() - 1),
+                            self.modulus,
+                        )
+                        shares, v, c = shamir.split(secret, k, n, salt)
+                        points = list(
+                            zip(map(secret.coerce, itertools.count(1)), shares)
+                        )
+                        for i in range(10):
+                            with self.subTest(i=i):
+                                subset = random.sample(points, k)
+                                self.assertEqual(
+                                    int(
+                                        shamir._lagrange_interpolate(subset, 0)
+                                    ),
+                                    int(secret),
+                                )
+                                for x in range(1, n + 1):
+                                    self.assertEqual(
+                                        int(
+                                            shamir._lagrange_interpolate(
+                                                subset, x
+                                            )
+                                        ),
+                                        int(shares[x - 1]),
+                                    )
